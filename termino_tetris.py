c0b80ccb9644c4692d0b376707bdd781c78830e1
Rafi Abdullah <147731776+rafibuilds@users.noreply.github.com>
2024-06-03 12:31:31 +0600

    Update termino_tetris.py

--- termino_tetris.py
+++ termino_tetris.py
@@ -1,9 +1,23 @@
 '''
-A program to play tetris on the terminal/console.
+A game to play tetris in the terminal/console.
 
-To play this program on Windows OS:
-(type in the terminal)
+**For playing in windows**
 pip install windows-curses
+
+Playing instruction:
+(For simplicity, the arrow keys are used. W, A, S, D will not work)
+
+    Right arrow key ->
+    for going right
+
+    Left arrow key <-
+    for going left
+
+    Up arrow key ^ (not the carret char)
+    for rotating the block
+
+    Space bar |__|
+    for droping the block
 '''
 import curses # The main module needed for this game to work
 import time # Time related functions for the smooth animation of the game
@@ -63,22 +77,22 @@ def draw_board(window, board):
     board[y][x][0] is the char
     board[y][x][1] is the color
     '''
-    for y in range(BOARD_HEIGHT):
-        for x in range(BOARD_WIDTH):
+    for y in range(BOARD_HEIGHT): # Go through every row of the board
+        for x in range(BOARD_WIDTH): # Go through every column of the row
             if board[y][x][0] == BOARD_CHAR:
-                window.addstr(y, x, BOARD_CHAR)
+                window.addstr(y, x, BOARD_CHAR) # If it is not a block char, then no color
             elif board[y][x][0] == BLOCK_CHAR:
                 window.addstr(y, x, BLOCK_CHAR, board[y][x][1]) # If it is a block char, then there will be color
 
 def draw_score(window, score):
     '''Draws the score board for the game'''
-    height, width = window.getmaxyx()
-    scr_text = f"Score: {score}"
-    y, x = (height // 2), (width // 2 - len(scr_text) // 2)
-    window.addstr(y, x, scr_text)
+    height, width = window.getmaxyx() # Get the height, width of the window
+    scr_text = f"Score: {score}" # Create the score text
+    y, x = (height // 2), (width // 2 - len(scr_text) // 2) # Calculate the y, x for the scr_text to go on the window
+    window.addstr(y, x, scr_text) # Print the scr_text on the window
 
 def draw_instruction(window):
-    '''Draws the instructions to play the game'''
+    '''Draws the instructions'''
     window.addstr(0, 0, 'Move right:')
     window.addstr(1, 0, 'RIGHT Arrow Key ->')
     window.addstr(2, 0, '------------------')
@@ -105,17 +119,16 @@ def make_board_list(rows, cols, char):
     board_data_list = []
     # Create the board data structure
     for _ in range(rows):
-        board_data_list.append([(char, None)] * cols) # (char, None), None is replacent for the color
+        board_data_list.append([(char, None)] * cols) # (char, None); None is replacent for the color
     return board_data_list
 
 def rotate_block(shape):
     '''Rotates the block counter-clockwise'''
-    width, height = len(shape[0]), len(shape)
-
+    width, height = len(shape[0]), len(shape) # Get the height, width of the original block
     # In one rotation the height will be the width and the width will be the height
-    new_block = [[None] * height for _ in range(width)]
+    new_block = [[None] * height for _ in range(width)] # Create a new block whose height will be width of the original block and vice versa
     for col in range(width):
-        for row in range(height - 1, -1, -1):
+        for row in range(height - 1, -1, -1): # Iterate from the last row of the original block
             new_block[width - col - 1][row] = shape[row][col]
     return new_block
 
@@ -126,7 +139,7 @@ def draw_block(board, block, blocky, blockx, color):
         for col, piece in enumerate(part):
             if piece == 1:
                 board[blocky + row][blockx + col] = (BLOCK_CHAR, color)
-            elif piece == 0:
+            elif piece == 0: # No need to put that on the board
                 continue
     return board
 
@@ -146,7 +159,6 @@ def colliding(board, block, blocky, blockx):
     return False
 
 def drop_block(board, block, blocky, blockx, max_vertical):
-    '''Drops the block'''
     y = blocky
     while not colliding(board, block, y, blockx):
         y += 1
@@ -158,9 +170,9 @@ def drop_block(board, block, blocky, blockx, max_vertical):
 def score_and_clear(board, score):
     '''Clears the rows and scores, if all the chars are BLOCK_CHAR'''
     while True:
-        if all(item[0] == BLOCK_CHAR for item in board[-1]):
+        if all(item[0] == BLOCK_CHAR for item in board[-1]): # Checks the last row
             del board[-1]
-            board.insert(0, [(BOARD_CHAR, None)] * BOARD_WIDTH)
+            board.insert(0, [(BOARD_CHAR, None)] * BOARD_WIDTH) # inserts a new row at the first index
             score += 1
         else:
             break
@@ -204,7 +216,7 @@ def main(stdscr):
 
     # If the screen is smaller than the board, the program will simply not run
     if (board_win_width + instr_win_width + 5) > screen_width or (board_win_height + score_win_height + 1) > screen_height:
-        sys.exit("\nPlease make the terminal full screen or just a bit bigger, buddy\n")
+        sys.exit("\nPlease make the terminal full screen or just a bit bigger, buddy")
 
     # Create all the windows
     board_win = curses.newwin(board_win_height, board_win_width, board_win_y, board_win_x) # Create the board window
@@ -243,7 +255,7 @@ def main(stdscr):
                     board_list_copy = copy.deepcopy(board_list) # If a block is colliding with another block, then it is another round
                     break
             else:
-                game_active = False # The game will not generate any more blocks
+                game_active = False
                 score_brd_win.clear()
                 score_brd_win.refresh()
                 instr_win.clear()
@@ -251,7 +263,7 @@ def main(stdscr):
                 break
 
             # Update the score
-            score, board_list = score_and_clear(board_list, score) # Update the socre, clear the rows
+            score, board_list = score_and_clear(board_list, score)
 
             # Draw the main game window
             board_win.clear() # Clear the window
@@ -279,7 +291,7 @@ def main(stdscr):
                     board_list = copy.deepcopy(board_list_copy)
                 else:
                     board_list = make_board_list(BOARD_HEIGHT, BOARD_WIDTH, BOARD_CHAR)
-            if y > maximum_y: # If y is out of the board height, then copy the previous state and break the loop
+            if y > maximum_y:
                 board_list_copy = copy.deepcopy(board_list)
                 break
 
@@ -288,8 +300,8 @@ def main(stdscr):
             # Block movements
             # Quit
             if key == ord('q') or key == ord('Q'):
-                game_active = False # The game will not generate any more blocks
-                # Clear the instruction and score windows and break out of the loop
+                # Clear the score and instruction window and break out of the loop
+                game_active = False
                 score_brd_win.clear()
                 score_brd_win.refresh()
                 instr_win.clear()
@@ -298,16 +310,16 @@ def main(stdscr):
             # Move the block right -->
             if key == curses.KEY_RIGHT and x <= maximum_x and y <= maximum_y and not colliding(board_list, block, y, x):
                 x += 1
-                if x > maximum_x: # Check if x is greater than the maximum range of x
+                if x > maximum_x:
                     x = maximum_x
-                if colliding(board_list, block, y, x): # Check if the increase in x makes the block collide with another block
+                if colliding(board_list, block, y, x): # If increasing the x makes the block collide with another one, decrease it
                     x -= 1
             # Move the block left <--
             if key ==  curses.KEY_LEFT and x >= 0 and y <= maximum_y and not colliding(board_list, block, y, x):
                 x -= 1
-                if x < 0: # Check if x is less than 0
+                if x < 0:
                     x = 0
-                if colliding(board_list, block, y, x): # Check if the decrease in x makes the block collide with another block
+                if colliding(board_list, block, y, x): # If decreasing the x makes the block collide with another one, increase it
                     x += 1
             # Rotate the block counter-clockwise
             if key == curses.KEY_UP and (0 <= y < maximum_y) and (0 <= x <= maximum_x) and not colliding(board_list, block, y, x):
@@ -325,21 +337,25 @@ def main(stdscr):
                 if not colliding(board_list, temp_block, y, x):
                     # Copy that shit
                     block = copy.deepcopy(temp_block)
-                    del temp_block
+                    del temp_block # Delete the temporary one
                 else:
-                    del temp_block
+                    del temp_block # Delete the temporary one because the block cannot be rotated
             # Drop the block
-            if key == ord(' ') and y < maximum_y:
-                y = drop_block(board_list, block, y, x, maximum_y)
+            if key == ord(' ') and (y < maximum_y):
+                y = drop_block(board_list, block, y, x, maximum_y) # Get the coordiante of where the block will drop
 
             # Slow the program for smooth animation
             time.sleep(LEVEL)
 
-    # If the game ends or quits, show a 'GAME OVER!' text
+    # If the game ends, show a text that says "GAME OVER!" and show the score
     board_win.clear()
     board_win.addstr(board_win_height // 2, 0, 'GAME OVER!')
     board_win.refresh()
-    time.sleep(1)
+    time.sleep(1) # Stop the program for a while to display the "GAME OVER!" text
+    board_win.clear()
+    draw_score(board_win, score) # Draw the score
+    board_win.refresh()
+    time.sleep(0.5) # Pause for a little to show the score
 
 if __name__ == "__main__":
     curses.wrapper(main)
